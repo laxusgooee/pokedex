e925f4ad2eea1f78ae532ececb74177431cf944c
Gbenga Medunoye <laxusgooee@gmail.com>
2024-07-12 22:47:58 +0100

    cleanup: remove some unused files

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PokemonController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', [PokemonController::class, 'index'])->name('pokemon.index');
 Route::get('/{name}', [PokemonController::class, 'show'])->name('pokemon.show');
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
